510e738071bae7cdd33346fb697611308bf1695e
Joshua <joshuadelgado073@gmail.com>
2025-07-23 08:50:00 +0800

    Organized Server.js file

--- backend/server.js
+++ backend/server.js
@@ -1,31 +1,35 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 require('dotenv').config();
-const authRoutes = require('./routes/auth');
 
+// Import Routes
+const authRoutes = require('./routes/auth');
+const jobRoutes = require('./routes/jobs');
+const userRoutes = require('./routes/user');
+const accountRoutes = require('./routes/account');
+const messagesRoute = require('./routes/messages');
+
+
+// Initialize App
 const app = express();
+
+// Middleware
 app.use(cors());
 app.use(express.json());
 
-app.use('/api/auth', authRoutes);
-
-app.get('/', (req, res) => res.send('API Running'));
-
-
-const jobRoutes = require('./routes/jobs');
-app.use('/jobs', jobRoutes);
-
-
-const userRoutes = require('./routes/user'); 
-app.use('/api/users', userRoutes); 
-
-
-const path = require('path');
+// Static Files
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
-const accountRoutes = require('./routes/account');
+// Routes
+app.get('/', (req, res) => res.send('API Running'));
+app.use('/api/auth', authRoutes);
+app.use('/jobs', jobRoutes);
+app.use('/api/users', userRoutes);
 app.use('/api/account', accountRoutes);
+app.use('/api/messages', messagesRoute);
 
 
+// Start Server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
