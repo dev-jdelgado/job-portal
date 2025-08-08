d7b2ff4ea32ae55731a86d2aafe1c8daa330c056
Joshua <joshuadelgado073@gmail.com>
2025-08-09 03:32:13 +0800

    update backend code for production

--- backend/db.js
+++ backend/db.js
@@ -4,5 +4,7 @@ const pool = mysql.createPool({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
+    port: process.env.DB_PORT, 
+    connectTimeout: 10000 
 });
 module.exports = pool.promise();
